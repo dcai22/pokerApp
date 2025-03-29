be41c516efad3c81bd38aafe24f3febc2802c4b5
dale <imsuchadale@gmail.com>
2025-03-30 01:48:19 +1100

    added yes and no options for button press

--- app/components/RandomButton.tsx
+++ app/components/RandomButton.tsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 
 
 function RandomButton() {
-    let [numClicks, setNumClicks] = useState(0);
+    let [numYes, setNumYes] = useState(0);
+    let [numNo, setNumNo] = useState(0);
     
     return (
+        // Yes percentage edge case when numYes = numNo = 0
         <>
-            <h1 onClick={() => setNumClicks(numClicks + 1)}>CLICK ME</h1>
-            <h1>{numClicks}</h1>
+            <h1 onClick={() => setNumYes(numYes + 1)}>VOTE YES</h1>
+            <h1 onClick={() => setNumNo(numNo + 1)}>VOTE NO</h1>
+            <h1>Yes Votes: {numYes}</h1>
+            <h1>Yes Percentage: {numYes / ((numNo + numYes) || 1)}</h1>
+            <h1 onClick={() => { setNumYes(0); setNumNo(0); }}>RESET</h1>
         </>
     );
 }
