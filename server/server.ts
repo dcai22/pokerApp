5413aed80561dc1971961c49e378b1aa0ebe69a3
dale <imsuchadale@gmail.com>
2025-03-31 00:26:08 +1100

    connected to mongodb

--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,10 @@ const connect = require("./connect");
 const app = require("./app");
 const port = 3000;
 
+import { connectToServer, getDb } from "./connect";
+
 // Start server
-app.listen(port, () => {
-    connect.connectToServer();
+app.listen(port, async () => {
+    connectToServer();
     console.log(`Server is running on http://localhost:${port}`);
 });
