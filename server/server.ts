5cf2f6ac80bea47a52ffcd0f90997a0cee0d414e
dale <imsuchadale@gmail.com>
2025-03-30 23:21:57 +1100

    added connection to mongodb

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
+const connect = require("./connect");
 const app = require("./app");
 const port = 3000;
 
 // Start server
 app.listen(port, () => {
+    connect.connectToServer();
     console.log(`Server is running on http://localhost:${port}`);
 });
