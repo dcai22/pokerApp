20744ee6a25452ee5f227702ea9d693702618b3b
dale <imsuchadale@gmail.com>
2025-03-30 23:20:13 +1100

    implemented yes and no buttons

--- app/components/RandomButton.tsx
+++ app/components/RandomButton.tsx
@@ -10,7 +10,7 @@ function RandomButton() {
         <>
             <h1 onClick={() => setNumYes(numYes + 1)}>VOTE YES</h1>
             <h1 onClick={() => setNumNo(numNo + 1)}>VOTE NO</h1>
-            <h1>Yes Votes: {numYes}</h1>
+            <h1>Yes Votes: {numYes} / {numNo + numYes}</h1>
             <h1>Yes Percentage: {numYes / ((numNo + numYes) || 1)}</h1>
             <h1 onClick={() => { setNumYes(0); setNumNo(0); }}>RESET</h1>
         </>
