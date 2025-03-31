33e23d9437ed810504be1302b37e10ebe25b4703
dale <imsuchadale@gmail.com>
2025-03-31 17:29:37 +1100

    successfully connected to psql

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,8 @@
 import express, { json, type Request, type Response } from 'express';
 import cors from 'cors';
 
+import pool from "./db";
+
 const app = express();
 
 app.use(json());
@@ -47,4 +49,64 @@ app.get('/player/getVpip', (req: Request, res: Response) => {
 
 app.get('/player/handStats', (req: Request, res: Response) => {
     // TODO;
+});
+
+
+// FOR TESTING
+app.get('/numVotes', async (req: Request, res: Response) => {
+    try {
+        const allVotes = await pool.query("SELECT * FROM test WHERE id=1");
+        res.json(allVotes.rows[0]);
+        console.log(allVotes.rows[0]);
+    } catch(err) {
+        console.log(err);
+    }
+});
+// app.put('/voteYes', async (req: Request, res: Response) => {
+//     try {
+//         const allVotes = await pool.query("SELECT * FROM test WHERE id=1");
+//         await pool.query(
+//             "UPDATE test SET voteYes=$1",
+//             [allVotes.rows[0].voteyes + 1]
+//         );
+//         res.json("res was updated");
+//     } catch(err) {
+//         console.log(err);
+//     }
+// });
+// app.put('/voteNo', async (req: Request, res: Response) => {
+//     try {
+//         const allVotes = await pool.query("SELECT * FROM test WHERE id=1");
+//         await pool.query(
+//             "UPDATE test SET voteNo=$1",
+//             [allVotes.rows[0].voteno + 1]
+//         );
+//         res.json("res was updated");
+//     } catch(err) {
+//         console.log(err);
+//     }
+// });
+// app.put('/voteReset', async (req: Request, res: Response) => {
+//     try {
+//         await pool.query(
+//             "UPDATE test SET voteYes=$1 voteNo=$2",
+//             [0, 0]
+//         );
+//         res.json("res was updated");
+//     } catch(err) {
+//         console.log(err);
+//     }
+// });
+app.put('/updateVotes', async (req: Request, res: Response) => {
+    const numYes = req.body.numYes as number;
+    const numNo = req.body.numNo as number;
+    try {
+        await pool.query(
+            "UPDATE test SET numYes=$1, numNo=$2 WHERE id=1",
+            [numYes, numNo]
+        );
+        res.json("res was updated");
+    } catch(err) {
+        console.log(err);
+    }
 });
