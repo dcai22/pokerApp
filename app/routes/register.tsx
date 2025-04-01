10cbc3ae48eac69ac3e3c86ae14784f2ebc3625c
dale <imsuchadale@gmail.com>
2025-04-01 15:15:29 +1100

    added an input field for name

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,11 +1,27 @@
-function Register() {
+import { Form } from "react-router";
+import type { Route } from "../+types/root";
+import axios from "axios";
+
+export async function action({ request, params }: Route.ActionArgs) {
+    const formData = await request.formData();
+    const updates = Object.fromEntries(formData);
+    console.log(updates.username);
+}
+
+export default function Register() {
     return (
         <>
-            <p>
-                What is your name?
-            </p>
+            <div>
+                Please enter your name:
+            </div>
+            <Form method="post">
+                <div>
+                    <input name="username" type="text"></input>
+                </div>
+                <button type="submit">
+                    SUBMIT
+                </button>
+            </Form>
         </>
     );
 }
-
-export default Register;
