65276094c146fcea35dc3209f45b380418ce9257
dale <imsuchadale@gmail.com>
2025-04-07 00:20:42 +1000

    prettified register page

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -3,6 +3,8 @@ import type { Route } from "../+types/root";
 import axios from "axios";
 import { genHash } from "server/helpers/auth";
 import { commitSession, getSession } from "~/sessions.server";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const session = await getSession(request.headers.get("Cookie"));
@@ -64,18 +66,17 @@ export default function Register() {
     const navigate = useNavigate();
 
     return (
-        <>
-            Register a new account<br />
-            <Form method="post">
-                <label htmlFor="username">Username:</label><br />
-                <input name="username" id="username" type="text"></input><br />
-
-                <label htmlFor="password">Password:</label><br />
-                <input name="password" id="password" type="password"></input><br />
-
-                <button type="submit">Submit</button>
-            </Form>
-            Already have an account? Login <button onClick={() => navigate("/login")}>HERE</button><br />
-        </>
+        <div className="flex flex-col justify-center items-center w-screen h-screen">
+            <div className="flex flex-col justify-center">
+                <h1 className="mb-2">Register a PokerApp account:</h1>
+                <Form className="flex flex-col" method="post">
+                    <Input placeholder="Username" name="username" type="text" className="mb-2"></Input>
+                    <Input placeholder="Password" name="password" type="password" className="mb-2"></Input>
+                    <Button type="submit" className="mb-10">Register</Button>
+                </Form>
+                <h1 className="mb-1">Already have an account?</h1>
+                <Button onClick={() => navigate("/login")}>Login here</Button>
+            </div>
+        </div>
     );
 }
