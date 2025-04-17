15703fd19e01ab225ff48b2fffd64cf011d2a8ef
dale <imsuchadale@gmail.com>
2025-04-17 22:51:41 +1000

    added minor aesthetic changes to graying out names when offline

--- app/components/PositionsDisplay.tsx
+++ app/components/PositionsDisplay.tsx
@@ -14,9 +14,9 @@ export default function PositionsDisplay({ players, ownerName, username }: Posit
             <div className="mb-10">
                 <ul>
                     {players.map((e, i) => 
-                        <li key={i} className="flex">
+                        <li key={i} className={`flex ${e.isActive ? "" : "text-gray-400"}`}>
                             <span className="w-7">{e.name === ownerName ? "⭐" : ""}</span>
-                            <span className={`w-15 ${e.isActive ? "font-bold" : "text-gray-500"}`}>{calcPosition(i, players.length)}</span>
+                            <span className="w-15 font-bold">{calcPosition(i, players.length)}</span>
                             <span className={`${e.name === username ? "underline" : ""}`}>{e.name}</span>
                         </li>
                     )}
