cb962ec5bd7b2111f7351c4443d109fa83f44cdc
dale <imsuchadale@gmail.com>
2025-04-19 15:39:01 +1000

    aesthetic changes to hand and vpip selection

--- app/components/SuitSelect.tsx
+++ app/components/SuitSelect.tsx
@@ -1,29 +1,38 @@
 import { Card } from "server/interface";
-import { FormControl, FormItem, FormLabel } from "./ui/form";
-import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { FormControl, FormItem } from "./ui/form";
+import { RadioGroup } from "./ui/radio-group";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 import { shiftArray } from "~/helpers";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 interface SuitSelectProps {
-    onValueChange: ((value: string) => void) | undefined,
+    onValueChange: ((value: string) => void),
     offset: number,
 }
 
 export default function SuitSelect({ onValueChange, offset }: SuitSelectProps) {
+    const [value, setValue] = useState("");
+
     return (
         <RadioGroup
-            onValueChange={onValueChange}
+            onValueChange={(v) => {
+                setValue(v);
+                onValueChange(v);
+            }}
             defaultValue=""
-            className="flex flex-col space-y-1"
+            className="flex flex-col space-y-1 gap-1"
         >
             {shiftArray(Card.suits, offset)
-                .map((e, i) => 
+                .map((e, i) =>
                     <FormItem className="flex items-center space-x-3 space-y-0" key={i}>
-                        <FormControl>
-                            <RadioGroupItem value={e} />
+                        <FormControl className="checked:bg-black">
+                            <RadioGroupPrimitive.Item value={e}>
+                                <Button className={` ${["d", "h"].includes(e) ? "text-red-500" : "text-black"} ${value === e ? "bg-gray-500 text-white" : "bg-white"} border-1 border-gray-200 shadow-xs hover:bg-gray-500 hover:text-white w-15`}>
+                                    {Card.prettySuit(e)}
+                                </Button>
+                            </RadioGroupPrimitive.Item>
                         </FormControl>
-                        <FormLabel className={`font-normal ${["d", "h"].includes(e) ? "text-red-500" : "text-black" }`}>
-                            {Card.prettySuit(e)}
-                        </FormLabel>
                     </FormItem>
                 )
             }
